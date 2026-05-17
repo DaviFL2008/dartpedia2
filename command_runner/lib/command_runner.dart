fae3ce5d28dbb9a1620c3649e3c8fb6ef372e3f3
LFN-FENIX <lucasnovaes479@gmail.com>
2026-05-17 20:58:54 -0300

    Create command_runner.dart

---
+++ command_runner/lib/command_runner.dart
@@ -0,0 +1,17 @@
+// ----------------------------------------------------- Feito por Thiago ------------------------------------------------------------
+
+/* 
+	 Um corredor de comando simples para lidar com argumentos de linha de comando.
+
+	 Documentação mais extensa para esta biblioteca vai aqui 
+
+*/
+
+	library; 				// Declara este arquivo como uma biblioteca, que define fronteiras e a interface pública de uma unidade reutilizavel de código Dart.
+
+	export 'src/command_runner_base.dart';	// Declarações de command_runner_base.dart disponível para outros pacotes que importam o comand... Sem isto exportação enunciado, as classes e funções dentro de command_runner_base.dart
+						//  seria particular para o the comando_ runner pacote
+
+//	  Exportar quaisquer outras bibliotecas destinadas a clientes deste pacote. 
+
+// ----------------------------------------------------- Feito por Thiago ------------------------------------------------------------
