abdc8d84f6b9cf977fedcac009cd915ef80ce07d
Davi Ferreira Leite <daviferreiraleite2008@gmail.com>
2026-06-02 16:40:58 -0300

    Licao 06- Tarefa 4: Atualizar as exportações da biblioteca command_runner

--- command_runner/lib/command_runner.dart
+++ command_runner/lib/command_runner.dart
@@ -15,3 +15,16 @@
 //	  Exportar quaisquer outras bibliotecas destinadas a clientes deste pacote. 
 
 // ----------------------------------------------------- Feito por Thiago ------------------------------------------------------------
+
+//-----------------------------------------------Feito por Davi Ferreira Leite---------------------------------------------------
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/arguments.dart';
+export 'src/command_runner_base.dart';
+export 'src/help_command.dart';
+export 'src/exceptions.dart'; // Add this line
+
+// TODO: Export any libraries intended for clients of this package.
